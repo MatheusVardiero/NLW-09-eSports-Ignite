8f17ca4798cea053eeedddc96a7fdf0f43568eed
MatheusVardiero <matheusvardiero13@gmail.com>
2024-02-24 17:54:16 -0300

    configurando prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 //Importando o express para utiliza-lo
-import express from 'express';
+import express, { request } from 'express';
 
 //Criando a variável de utilizaçao
 const app = express();
@@ -8,13 +8,26 @@ const app = express();
 //Primeiro parametro passa o nome da rota
 //Segundo parametro request para fazer uma requisiçao de dados,
 //response para enviar uma resposta.
-app.get('/ads', (request, response) => {
+
+app.get('/games',(request, response) => {
+  return response.json([]);
+})
+
+app.post('/ads',(request, response) => {
+  return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
     { id: 3, name: 'Anúncio 3' },
     { id: 4, name: 'Anúncio 4' },
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([])
 })
 
 app.listen(3333);
